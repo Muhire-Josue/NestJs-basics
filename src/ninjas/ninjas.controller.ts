a571d1379eb30cef668f50d7a59cb7e1a5429960
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-09-18 09:57:13 +0200

    - Adds post endpoint

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
   @Get(':id')
   getOneNinja(@Param('id') id: string) {
     return { id };
+  }
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
   }
 }
